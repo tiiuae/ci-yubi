2c071a472539ee36e65ce93ebbaa5da6e0673804
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2025-06-29 21:57:08 +0300

    Secureboot Pipeline demo
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

--- secboot/signme.sh
+++ secboot/signme.sh
@@ -12,22 +12,33 @@ err_report() {
 trap 'err_report $LINENO' ERR
 
 # Input and constants
-if [[ $# -ne 1 ]]; then
-    log "[!] Usage: $0 <disk-image.zst>"
+if [[ $# -ne 3 ]]; then
+    log "[!] Usage: $0 <certificate> <disk-image.zst> <subdir>"
     exit 1
 fi
 
-DISK_IMAGE_ZST="$1"
+SUBDIR="$3"
+DISK_IMAGE_ZST="$2"
 DISK_IMAGE="disk.raw"
+DISK_IMAGE_ISO="disk.iso"
 EFI_IMAGE="efi-partition.img"
 SIGNED_EFI="BOOTX64.EFI.signed"
-CERT="uefi-signing-cert.pem"
+CERT="$1"
 KEY="vault:ghaf-secureboot-testkv:uefi-signing-key"
 REPO="harbor.ppclabz.net/ghaf-secboot/ghaf-uefi"
 TAG="signed"
 ZSTD_IMAGE="ghaf_0.0.1.raw.zst"
 
-case "$1" in
+export AZURE_CLI_ACCESS_TOKEN=$(curl -s \
+  'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://vault.azure.net' \
+  -H "Metadata: true" | jq -r .access_token)
+
+
+log "[DEBUG] cert: $1"
+log "[DEBUG] image: $2"
+log "[DEBUG] $# args remaining"
+
+case "$DISK_IMAGE_ZST" in
     *.iso)
 	input_type="iso"
 	log "ISO Image detected"
@@ -43,7 +54,7 @@ case "$1" in
 esac
 
 # Ensure required tools are available
-for cmd in zstd fdisk dd mcopy sbsign oras; do
+for cmd in zstd fdisk dd mcopy sbsign; do
     if ! command -v "$cmd" &>/dev/null; then
         log "[!] Required tool '$cmd' not found in PATH"
         exit 1
@@ -52,10 +63,10 @@ done
 
 # Jenkins global credentials / Jenkins secrets are used for now
 # TODO: Consider more secure approach
-if [[ -z "${ORAS_USERNAME:-}" || -z "${ORAS_PASSWORD:-}" ]]; then
-    log "[!] ORAS_USERNAME and ORAS_PASSWORD must be set"
-    exit 1
-fi
+#if [[ -z "${ORAS_USERNAME:-}" || -z "${ORAS_PASSWORD:-}" ]]; then
+#    log "[!] ORAS_USERNAME and ORAS_PASSWORD must be set"
+#    exit 1
+#fi
 
 log "[*] Cleaning up any previous artifacts..."
 rm -f "$DISK_IMAGE" "$EFI_IMAGE" "$SIGNED_EFI" BOOTX64.EFI
@@ -64,8 +75,10 @@ if [[ "$input_type" == "zst" ]]; then
     log "[*] Decompressing image: $DISK_IMAGE_ZST -> $DISK_IMAGE"
     zstd -d "$DISK_IMAGE_ZST" -o "$DISK_IMAGE"
 else
-    DISK_IMAGE=$DISK_IMAGE_ZST
+    cp $DISK_IMAGE_ZST $DISK_IMAGE_ISO
+    DISK_IMAGE=$DISK_IMAGE_ISO
 fi
+log "[*] Disk image: $DISK_IMAGE"
 chmod 666 "$DISK_IMAGE"
 
 log "[*] Locating EFI partition offset and size..."
@@ -89,9 +102,14 @@ if ! mcopy -i "$EFI_IMAGE" ::EFI/BOOT/BOOTX64.EFI BOOTX64.EFI; then
 fi
 
 log "[*] Signing BOOTX64.EFI..."
-if ! sbsign --engine e_akv --keyform engine --key "$KEY" --cert "$CERT" --output "$SIGNED_EFI" BOOTX64.EFI; then
-    log "[!] Failed to sign BOOTX64.EFI"
-    exit 1
+
+log "[DEBUG] Running: sbsign --engine e_akv --keyform engine --key \"$KEY\" --cert \"$CERT\" --output \"$SIGNED_EFI\" BOOTX64.EFI"
+sbsign --engine e_akv --keyform engine --key "$KEY" --cert "$CERT" --output "$SIGNED_EFI" BOOTX64.EFI 2>&1 | tee /tmp/sbsign.log
+ret=$?
+if [[ $ret -ne 0 ]]; then
+    log "[!] sbsign failed (exit code $ret)"
+    cat /tmp/sbsign.log
+    exit $ret
 fi
 
 log "[*] Inserting signed BOOTX64.EFI back into EFI image..."
@@ -105,14 +123,25 @@ dd if="$EFI_IMAGE" of="$DISK_IMAGE" bs=512 seek="$EFI_START" conv=notrunc status
 
 log "[+] Signed image is ready in $DISK_IMAGE"
 
-SIGNED_ZST="signed_$ZSTD_IMAGE"
-log "[*] Recompressing signed image to $SIGNED_ZST..."
-zstd -f "$DISK_IMAGE" -o "$SIGNED_ZST"
+if [[ "$input_type" == "zst" ]]; then
+    SIGNED_ZST="signed_$ZSTD_IMAGE"
+    log "[*] Recompressing signed image to $SIGNED_ZST..."
+    zstd -f "$DISK_IMAGE" -o "$SIGNED_ZST"
+    log "[*] Move signed image to $SUBDIR"
+    mkdir -p $SUBDIR
+    mv $SIGNED_ZST $SUBDIR
+else
+    log "[*] Move signed image to $SUBDIR"
+    mkdir -p $SUBDIR
+    mv $DISK_IMAGE_ISO $SUBDIR
+fi
 
-log "[*] Logging into OCI registry..."
-oras login harbor.ppclabz.net -u "$ORAS_USERNAME" -p "$ORAS_PASSWORD"
 
-log "[*] Pushing $SIGNED_ZST to OCI registry as $REPO:$TAG..."
-oras push "$REPO:$TAG" "$SIGNED_ZST:application/octet-stream"
+#log "[*] Logging into OCI registry..."
+#oras login harbor.ppclabz.net -u "$ORAS_USERNAME" -p "$ORAS_PASSWORD"
 
-log "[+] Success! Image uploaded as $REPO:$TAG"
+#log "[*] Pushing $SIGNED_ZST to OCI registry as $REPO:$TAG..."
+#oras push "$REPO:$TAG" "$SIGNED_ZST:application/octet-stream"
+
+#log "[+] Success! Image uploaded as $REPO:$TAG"
+log "[+] EFI Signing Success!"
