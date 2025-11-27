6d74de477d65a0ef3978628497ae5a5ee50aed6b
Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>
2025-11-27 12:40:55 +0200

    Remove unnecessary code  - remove CRT to PEM conversion as CRT is PEM
    Signed-off-by: Aleksandr Tserepov-Savolainen <aleksandr.tserepov-savolainen@unikie.com>

--- secboot/keygen.sh
+++ secboot/keygen.sh
@@ -32,7 +32,7 @@ openssl genrsa -out "${PK_DIR}/PK.key" 2048
 # Create self-signed certificate for PK
 openssl req -new -x509 -days 3650 \
     -key "${PK_DIR}/PK.key" \
-    -out "${PK_DIR}/PK.crt" \
+    -out "${PK_DIR}/PK.pem" \
     -config "${CONF}/create_PK_cert.ini"
 
 # --- Key Exchange Key (KEK) ---
@@ -46,10 +46,10 @@ openssl req -new \
 # Sign KEK CSR with PK (acts as CA)
 openssl x509 -req \
     -in "${KEK_DIR}/KEK.csr" \
-    -CA "${PK_DIR}/PK.crt" \
+    -CA "${PK_DIR}/PK.pem" \
     -CAkey "${PK_DIR}/PK.key" \
     -CAcreateserial \
-    -out "${KEK_DIR}/KEK.crt" \
+    -out "${KEK_DIR}/KEK.pem" \
     -days 3650 \
     -extfile "${CONF}/sign_KEK_csr.ini" \
     -extensions v3_req
@@ -65,20 +65,15 @@ openssl req -new \
 # Sign DB CSR with KEK
 openssl x509 -req \
     -in "${DB_DIR}/db.csr" \
-    -CA "${KEK_DIR}/KEK.crt" \
+    -CA "${KEK_DIR}/KEK.pem" \
     -CAkey "${KEK_DIR}/KEK.key" \
     -CAcreateserial \
-    -out "${DB_DIR}/db.crt" \
+    -out "${DB_DIR}/db.pem" \
     -days 3650 \
     -extfile "${CONF}/sign_DB_csr.ini" \
     -extensions v3_req
 
 # --- Export DER ---
-openssl x509 -in "${PK_DIR}/PK.crt" -outform DER -out "${PK_DIR}/PK.der"
-openssl x509 -in "${KEK_DIR}/KEK.crt" -outform DER -out "${KEK_DIR}/KEK.der"
-openssl x509 -in "${DB_DIR}/db.crt" -outform DER -out "${DB_DIR}/db.der"
-
-# --- Export PEM ---
-openssl x509 -in "${PK_DIR}/PK.crt" -outform PEM -out "${PK_DIR}/PK.pem"
-openssl x509 -in "${KEK_DIR}/KEK.crt" -outform PEM -out "${KEK_DIR}/KEK.pem"
-openssl x509 -in "${DB_DIR}/db.crt" -outform PEM -out "${DB_DIR}/db.pem"
+openssl x509 -in "${PK_DIR}/PK.pem" -outform DER -out "${PK_DIR}/PK.der"
+openssl x509 -in "${KEK_DIR}/KEK.pem" -outform DER -out "${KEK_DIR}/KEK.der"
+openssl x509 -in "${DB_DIR}/db.pem" -outform DER -out "${DB_DIR}/db.der"
